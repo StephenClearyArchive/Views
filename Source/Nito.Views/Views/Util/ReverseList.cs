08fe60079bdd4112d760a653a585543141efd6be
Stephen Cleary <Stephen Cleary@localhost>
2012-03-30 14:39:42 -0400

    Added SourceListBase, and changed ReverseList/SliceList to use it.

--- Source/Nito.Views/Views/Util/ReverseList.cs
+++ Source/Nito.Views/Views/Util/ReverseList.cs
@@ -9,46 +9,15 @@ namespace Views.Util
     /// Reverses a source list.
     /// </summary>
     /// <typeparam name="T">The type of object contained in the list.</typeparam>
-    public sealed class ReverseList<T> : ListBase<T>
+    public sealed class ReverseList<T> : SourceListBase<T>
     {
-        /// <summary>
-        /// The source list.
-        /// </summary>
-        private readonly IList<T> source;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="ReverseList&lt;T&gt;"/> class over the specified source list.
         /// </summary>
         /// <param name="source">The source list.</param>
         public ReverseList(IList<T> source)
+            : base(source)
         {
-            this.source = source;
-        }
-
-        /// <summary>
-        /// Gets a value indicating whether this list is read-only. This list is read-only iff its source list is read-only.
-        /// </summary>
-        /// <returns>true if this list is read-only; otherwise, false.</returns>
-        public override bool IsReadOnly
-        {
-            get { return this.source.IsReadOnly; }
-        }
-
-        /// <summary>
-        /// Removes all elements from the list.
-        /// </summary>
-        public override void Clear()
-        {
-            this.source.Clear();
-        }
-
-        /// <summary>
-        /// Gets the number of elements contained in this list.
-        /// </summary>
-        /// <returns>The number of elements contained in this list.</returns>
-        protected override int DoCount()
-        {
-            return this.source.Count;
         }
 
         /// <summary>
