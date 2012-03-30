aa41fa23f6dc3ff66529bd6f22ca0d33fd93a80d
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-03-30 16:17:58 -0400

    Added GenericView.

---
+++ Source/Nito.Views/Views/Util/GenericList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Views.Util
+{
+    /// <summary>
+    /// Wraps a non-generic source list, treating it as a generic list.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    public sealed class GenericList<T> : ListBase<T>
+    {
+        /// <summary>
+        /// The wrapped non-generic list.
+        /// </summary>
+        private readonly System.Collections.IList source;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericList&lt;T&gt;"/> class with the specified source.
+        /// </summary>
+        /// <param name="source">The non-generic source list to wrap.</param>
+        public GenericList(System.Collections.IList source)
+        {
+            this.source = source;
+        }
+
+        /// <summary>
+        /// Returns a value indicating whether the collection itself may be updated, e.g., <see cref="Add"/>, <see cref="Clear"/>, etc.
+        /// </summary>
+        /// <returns>A value indicating whether the collection itself may be updated.</returns>
+        protected override bool CanUpdateCollection()
+        {
+            return !this.source.IsFixedSize;
+        }
+
+        /// <summary>
+        /// Returns a value indicating whether the elements within this collection may be updated, e.g., the index setter.
+        /// </summary>
+        /// <returns>A value indicating whether the elements within this collection may be updated.</returns>
+        protected override bool CanUpdateElementValues()
+        {
+            return !this.source.IsReadOnly;
+        }
+
+        /// <summary>
+        /// Removes all items from this list.
+        /// </summary>
+        public override void Clear()
+        {
+            this.source.Clear();
+        }
+
+        /// <summary>
+        /// Gets the number of elements contained in this list.
+        /// </summary>
+        /// <returns>The number of elements contained in this list.</returns>
+        protected override int DoCount()
+        {
+            return this.source.Count;
+        }
+
+        /// <summary>
+        /// Gets an element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to get. This index is guaranteed to be valid.</param>
+        /// <returns>The element at the specified index.</returns>
+        protected override T DoGetItem(int index)
+        {
+            return (T)this.source[index];
+        }
+
+        /// <summary>
+        /// Sets an element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to get. This index is guaranteed to be valid.</param>
+        /// <param name="item">The element to store in the list.</param>
+        protected override void DoSetItem(int index, T item)
+        {
+            this.source[index] = item;
+        }
+
+        /// <summary>
+        /// Inserts an element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index at which the element should be inserted. This index is guaranteed to be valid.</param>
+        /// <param name="item">The element to store in the list.</param>
+        protected override void DoInsert(int index, T item)
+        {
+            this.source.Insert(index, item);
+        }
+
+        /// <summary>
+        /// Removes an element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to remove. This index is guaranteed to be valid.</param>
+        protected override void DoRemoveAt(int index)
+        {
+            this.source.RemoveAt(index);
+        }
+    }
+}
