aa39cf6575a9499f2ff6feb30620f0c338277bbb
Stephen Cleary <Stephen Cleary@localhost>
2012-03-31 13:26:50 -0400

    Changed LINQ methods to be based on IView<T> instead.

---
+++ Source/Nito.Views/Views/IView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Views
+{
+    public interface IView<T>
+    {
+        IList<T> AsList();
+    }
+}
