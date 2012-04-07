778edbb6a857a6f15458d153ca53349738a07ba1
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-04-07 12:40:16 -0400

    Removed TODO comment for TakeLast/SkipLast, since this is covered by passing negative parameter values to Slice.

--- Source/Nito.Views/Views/ViewLinq.cs
+++ Source/Nito.Views/Views/ViewLinq.cs
@@ -397,6 +397,6 @@ namespace Views
             return source.Pad(Views.View.Repeat(value, count));
         }
 
-        // TODO: Randomize, TakeWhile/SkipWhile, OrderBy/ThenBy, TakeLast/SkipLast/Buffer (from Rx)
+        // TODO: Randomize, TakeWhile/SkipWhile, OrderBy/ThenBy, Buffer (from Rx)
     }
 }
