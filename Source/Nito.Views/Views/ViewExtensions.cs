188775eac9be15b3122cac61980dd689f3e86210
Stephen Cleary <Stephen Cleary@localhost>
2012-04-07 10:42:19 -0400

    Removed some view extensions that just surface IList<T> functionality and aren't particularly well-designed.

--- Source/Nito.Views/Views/ViewExtensions.cs
+++ Source/Nito.Views/Views/ViewExtensions.cs
@@ -22,54 +22,6 @@ namespace Views
         }
 
         /// <summary>
-        /// Adds an item to the end of this view.
-        /// </summary>
-        /// <typeparam name="T">The type of element observed by the view.</typeparam>
-        /// <param name="item">The object to add to this view.</param>
-        /// <exception cref="T:System.NotSupportedException">
-        /// This view is read-only.
-        /// </exception>
-        public static void Add<T>(this IView<T> view, T item)
-        {
-            (view as IList<T>).Add(item);
-        }
-
-        /// <summary>
-        /// Determines whether this view contains a specific value.
-        /// </summary>
-        /// <typeparam name="T">The type of element observed by the view.</typeparam>
-        /// <param name="item">The object to locate in this view.</param>
-        /// <returns>
-        /// true if <paramref name="item"/> is found in this view; otherwise, false.
-        /// </returns>
-        public static bool Contains<T>(this IView<T> view, T item)
-        {
-            return (view as IList<T>).Contains(item);
-        }
-
-        /// <summary>
-        /// Copies the elements of this view to an <see cref="T:System.Array"/>, starting at a particular <see cref="T:System.Array"/> index.
-        /// </summary>
-        /// <typeparam name="T">The type of element observed by the view.</typeparam>
-        /// <param name="array">The one-dimensional <see cref="T:System.Array"/> that is the destination of the elements copied from this view. The <see cref="T:System.Array"/> must have zero-based indexing.</param>
-        /// <param name="arrayIndex">The zero-based index in <paramref name="array"/> at which copying begins.</param>
-        /// <exception cref="T:System.ArgumentNullException">
-        /// <paramref name="array"/> is null.
-        /// </exception>
-        /// <exception cref="T:System.ArgumentOutOfRangeException">
-        /// <paramref name="arrayIndex"/> is less than 0.
-        /// </exception>
-        /// <exception cref="T:System.ArgumentException">
-        /// <paramref name="arrayIndex"/> is equal to or greater than the length of <paramref name="array"/>.
-        /// -or-
-        /// The number of elements in the source view is greater than the available space from <paramref name="arrayIndex"/> to the end of the destination <paramref name="array"/>.
-        /// </exception>
-        public static void CopyTo<T>(this IView<T> view, T[] array, int arrayIndex)
-        {
-            (view as IList<T>).CopyTo(array, arrayIndex);
-        }
-
-        /// <summary>
         /// Copies all elements from one view into another view. The elements are copied in index order.
         /// </summary>
         /// <typeparam name="T">The type of element observed by the view.</typeparam>
@@ -82,22 +34,6 @@ namespace Views
             var count = list.Count;
             for (int i = 0; i != count; ++i)
                 destinationList[i] = list[i];
-        }
-
-        /// <summary>
-        /// Removes the first occurrence of a specific object from this view.
-        /// </summary>
-        /// <typeparam name="T">The type of element observed by the view.</typeparam>
-        /// <param name="item">The object to remove from this view.</param>
-        /// <returns>
-        /// true if <paramref name="item"/> was successfully removed from this view; otherwise, false. This method returns false if <paramref name="item"/> is not found in this view.
-        /// </returns>
-        /// <exception cref="T:System.NotSupportedException">
-        /// This view is read-only.
-        /// </exception>
-        public static bool Remove<T>(this IView<T> view, T item)
-        {
-            return (view as IList<T>).Remove(item);
         }
 
         /// <summary>
