da93624f0e88166f2a5d9774c6da9efab6161d76
Stephen Cleary <Stephen Cleary@localhost>
2012-04-08 15:41:50 -0400

    Split "ViewLinq" into standard View operations and actual LINQ support.

--- Source/Nito.Views/Views/IView.cs
+++ Source/Nito.Views/Views/IView.cs
@@ -9,7 +9,7 @@ namespace Views
     /// A view of data that provides fast random-access to elements. Any type implementing this interface must also implement <see cref="IList{T}"/>. Views do not necessarily store data; they may be "virtual" in the sense of creating the data elements on request.
     /// </summary>
     /// <typeparam name="T">The type of item observed by the view.</typeparam>
-    public interface IView<T>
+    public interface IView<T> : Linq.IView
     {
     }
 }
