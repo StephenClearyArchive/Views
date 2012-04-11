ee954d5fedaeab343c3f4021089c3cdcdc4d5183
Stephen Cleary <Stephen Cleary@localhost>
2012-04-11 13:21:36 -0400

    Added project for unit tests.

---
+++ Source/Nito.Views/Unit Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Nito.Views Unit Tests")]
+[assembly: AssemblyCompany("Nito Programs")]
+[assembly: AssemblyProduct("Nito.Views")]
+[assembly: AssemblyCopyright("Copyright © 2009-2012 Nito Programs")]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Source/Nito.Views/Unit Tests/SourceListBase.GenericItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Views;
+
+namespace SourceListBase
+{
+    [TestClass]
+    public class GenericItem
+    {
+        [TestMethod]
+        [ExpectedException(typeof(Exception), AllowDerivedTypes = true)]
+        public void NegativeIndexThrowsException()
+        {
+            var view = new int[0].View() as IList<int>;
+            var ret = view[-1];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), AllowDerivedTypes = true)]
+        public void IndexEqualToCountThrowsException()
+        {
+            var view = new int[1].View() as IList<int>;
+            var ret = view[1];
+        }
+
+        [TestMethod]
+        public void GetReadsValue()
+        {
+            var view = new int[] { 13 }.View() as IList<int>;
+            Assert.AreEqual(13, view[0]);
+        }
+
+        [TestMethod]
+        public void SetWritesValue()
+        {
+            var array = new int[] { 13 };
+            var view = array.View() as IList<int>;
+            view[0] = 17;
+            Assert.AreEqual(17, array[0]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void SetOnReadOnlySourceThrowsException()
+        {
+            var list = new List<int> { 13 }.AsReadOnly();
+            var view = list.View() as IList<int>;
+            view[0] = 17;
+        }
+    }
+}
