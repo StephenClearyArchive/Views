2f5217d7b14c6083d52c57a1f10ae67837c86b28
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-04-13 13:21:50 -0400

    Added SequenceAssert (with a simplistic implementation for now) and basic tests for View.Empty.

---
+++ Source/Nito.Views/Unit Tests/SequenceAssert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+public static class SequenceAssert
+{
+    public static void AreEquivalent<T>(IEnumerable<T> expected, IEnumerable<T> actual)
+    {
+        CollectionAssert.AreEquivalent(expected.ToList(), actual.ToList());
+    }
+
+    public static void ForEach<T>(IEnumerable<T> result, Action<T> test)
+    {
+        foreach (var value in result)
+            test(value);
+    }
+}
---
+++ Source/Nito.Views/Unit Tests/View/Empty.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Views;
+
+namespace t.View
+{
+    [TestClass]
+    public class Empty
+    {
+        [TestMethod]
+        public void HasCountOf0()
+        {
+            var view = Views.View.Empty<int>();
+            Assert.AreEqual(0, view.Count());
+        }
+
+        [TestMethod]
+        public void HasEmptySequence()
+        {
+            var view = Views.View.Empty<int>();
+            SequenceAssert.AreEquivalent(new int[0], view as IList<int>);
+        }
+    }
+}
