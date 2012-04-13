91962891c904683078c724f52d0c39791456193b
Stephen Cleary <Stephen Cleary@localhost>
2012-04-13 12:04:19 -0400

    Changed tests to use Moles framework.

--- Source/Nito.Views/Unit Tests/CollectionChangedListener/Create.cs
+++ Source/Nito.Views/Unit Tests/CollectionChangedListener/Create.cs
@@ -5,51 +5,24 @@ using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Views.Util;
 using System.Collections.ObjectModel;
+using Views.Util.Moles;
 
 namespace t.CollectionChangedListener
 {
-    internal sealed class IntResponder : ICollectionChangedResponder<int>
-    {
-        void ICollectionChangedResponder<int>.Added(int index, int item)
-        {
-            this.Added(index, item);
-        }
-
-        void ICollectionChangedResponder<int>.Removed(int index, int item)
-        {
-            this.Removed(index, item);
-        }
-
-        void ICollectionChangedResponder<int>.Replaced(int index, int oldItem, int newItem)
-        {
-            this.Replaced(index, oldItem, newItem);
-        }
-
-        void ICollectionChangedResponder<int>.Reset()
-        {
-            this.Reset();
-        }
-
-        public Action<int, int> Added { get; set; }
-        public Action<int, int> Removed { get; set; }
-        public Action<int, int, int> Replaced { get; set; }
-        public Action Reset { get; set; }
-    }
-
     [TestClass]
     public class Create
 	{
         [TestMethod]
         public void ReturnsNullIfSourceDoesNotImplementINotifyCollectionChanged()
         {
-            var listener = CollectionChangedListener<int>.Create(new int[0], new IntResponder());
+            var listener = CollectionChangedListener<int>.Create(new int[0], new SICollectionChangedResponder<int>());
             Assert.IsNull(listener);
         }
 
         [TestMethod]
         public void ReturnsInstanceifSourceImplementsINotifyCollectionChanged()
         {
-            var listener = CollectionChangedListener<int>.Create(new ObservableCollection<int>(), new IntResponder());
+            var listener = CollectionChangedListener<int>.Create(new ObservableCollection<int>(), new SICollectionChangedResponder<int>());
             Assert.IsNotNull(listener);
         }
     }
