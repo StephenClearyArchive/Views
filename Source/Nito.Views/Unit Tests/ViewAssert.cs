4ac1150500e9f584c6a61ed84fc95751c55e4e14
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-04-13 14:29:54 -0400

    Added ViewAssert.BasicFunctionality, which is not yet used.

--- Source/Nito.Views/Unit Tests/ViewAssert.cs
+++ Source/Nito.Views/Unit Tests/ViewAssert.cs
@@ -56,4 +56,39 @@ public static class ViewAssert
         ThrowsException(() => { list.RemoveAt(0); });
         ThrowsException(() => { list.Clear(); });
     }
+
+    public static void BasicFunctionality<T>(IView<T> view)
+    {
+        // Views implement both IList<T> and IList.
+        var list = view as IList<T>;
+        var objectList = view as System.Collections.IList;
+        Assert.IsNotNull(list);
+        Assert.IsNotNull(objectList);
+
+        // Both have the same count.
+        Assert.AreEqual(list.Count, objectList.Count);
+
+        // IList<T>.IsReadOnly = IList.IsReadOnly || IList.IsFixedSize
+        Assert.AreEqual(list.IsReadOnly, objectList.IsReadOnly || objectList.IsFixedSize);
+
+        // IsSynchronized is false, but SyncRoot is an independent object.
+        Assert.IsFalse(objectList.IsSynchronized);
+        Assert.IsNotNull(objectList.SyncRoot);
+        Assert.AreNotSame(view, objectList.SyncRoot);
+
+        // Invalid Item indexes are rejected.
+        ThrowsException(() => { var test = list[-1]; });
+        ThrowsException(() => { var test = list[list.Count]; });
+        ThrowsException(() => { var test = objectList[-1]; });
+        ThrowsException(() => { var test = objectList[list.Count]; });
+
+        // Both have the same elements.
+        AreEquivalent(list, objectList.Cast<T>());
+
+        // For each list, accessing via the indexer is the same as the enumerable implementation.
+        AreEquivalent(Enumerable.Range(0, list.Count).Select(i => list[i]), list);
+        AreEquivalent(Enumerable.Range(0, objectList.Count).Select(i => objectList[i]).Cast<T>(), list);
+
+        // TODO: Contains, IndexOf, CopyTo.
+    }
 }
