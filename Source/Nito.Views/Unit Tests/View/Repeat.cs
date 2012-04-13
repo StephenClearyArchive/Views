03b35e85523af2693f92635459894bcdec5725b3
Stephen Cleary <Stephen Cleary@localhost>
2012-04-13 14:06:42 -0400

    Basic tests for Repeat and Return.

---
+++ Source/Nito.Views/Unit Tests/View/Repeat.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Views;
+
+namespace t.View
+{
+    [TestClass]
+    public class Repeat
+    {
+        [TestMethod]
+        public void HasSpecifiedCount()
+        {
+            var view = Views.View.Repeat(3, 5);
+            Assert.AreEqual(5, view.Count());
+        }
+
+        [TestMethod]
+        public void IsEquivalentToEnumerableRepeat()
+        {
+            var view = Views.View.Repeat(3, 5);
+            ViewAssert.AreEquivalent(Enumerable.Repeat(3, 5), view);
+        }
+
+        [TestMethod]
+        public void ZeroCountIsEqualToEmpty()
+        {
+            var view = Views.View.Repeat(3, 0);
+            Assert.AreSame(Views.View.Empty<int>(), view);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), AllowDerivedTypes = true)]
+        public void NegativeCountThrowsException()
+        {
+            var view = Views.View.Repeat(3, -1);
+        }
+
+        [TestMethod]
+        public void ReadOnly()
+        {
+            var view = Views.View.Repeat(3, 5);
+            ViewAssert.CollectionIsReadOnly(view);
+            ViewAssert.ElementsAreReadOnly(view);
+        }
+    }
+}
---
+++ Source/Nito.Views/Unit Tests/View/Return.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Views;
+
+namespace t.View
+{
+    [TestClass]
+    public class Return
+    {
+        [TestMethod]
+        public void HasSpecifiedNumberOfItems()
+        {
+            var view = Views.View.Return(3, 5);
+            Assert.AreEqual(2, view.Count());
+        }
+
+        [TestMethod]
+        public void HasSpecifiedItems()
+        {
+            var view = Views.View.Return(3, 5);
+            ViewAssert.AreEquivalent(new[] { 3, 5 }, view);
+        }
+
+        [TestMethod]
+        public void ReadOnly()
+        {
+            var view = Views.View.Return(3, 5);
+            ViewAssert.CollectionIsReadOnly(view);
+            ViewAssert.ElementsAreReadOnly(view);
+        }
+    }
+}
