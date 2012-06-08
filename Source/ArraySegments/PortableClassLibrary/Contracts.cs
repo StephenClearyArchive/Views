e760e89d8a2cfda1a466fc07b5bf9a706b1008fb
Stephen Cleary <Stephen Cleary@localhost>
2012-06-08 10:47:07 -0400

    Added ArraySegments project.

---
+++ Source/ArraySegments/PortableClassLibrary/Contracts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Diagnostics.Contracts
+{
+    internal static class Contract
+    {
+        public static T Result<T>() { return default(T); }
+        public static T OldValue<T>(T value) { return value; }
+
+        [Conditional("never")]
+        public static void Ensures(bool condition) { }
+
+        [Conditional("never")]
+        public static void Requires(bool condition) { }
+
+        [Conditional("never")]
+        public static void Assume(bool condition) { }
+
+        [Conditional("never")]
+        public static void Assert(bool condition) { }
+
+        [Conditional("never")]
+        public static void Invariant(bool condition) { }
+    }
+
+    internal sealed class ContractInvariantMethodAttribute : Attribute
+    {
+    }
+}
---
+++ Source/ArraySegments/PortableClassLibrary/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyDescription("Extension methods for ArraySegment.")]
+[assembly: AssemblyCompany("Nito Programs")]
+[assembly: AssemblyCopyright("Copyright © 2012 Nito Programs")]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.0.0")]
